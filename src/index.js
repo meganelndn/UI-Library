d1cfd3a86239d5f6ab851d169ff387b0a4304ffb
Mégane <megane.londono@hotmail.com>
2021-12-08 14:42:16 +0100

    UI library done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
-import { styleBody, addTitle, contact } from "./dom";
-import users, { getPremUsers } from "./data";
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
-const premUsers = getPremUsers(users);
-console.log(users, premUsers);
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
 
-console.log("test2");
-console.log("test3");
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me :)');
+})
